e81e9ba6033326909682f947a93fa972377ce094
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2016-11-07 12:01:11 +1300

    update

--- demos/simple_flight/runtmux.sh
+++ demos/simple_flight/runtmux.sh
@@ -2,21 +2,24 @@
 LD_LIBRARY_PATH=/local/ori/GameEngines/UnrealEngine/Engine/Binaries/Linux/
 PATH="/local/ori/anaconda3/bin:$PATH"
 UE4PATH=/local/ori/GameEngines/UnrealEngine
-#PROJECT_NAME=testplugin
 PROJECT_NAME=testprj6
 PROJECT_PATH=/local/learn/ur4/${PROJECT_NAME}
 PROJECT_FILE=${PROJECT_PATH}/${PROJECT_NAME}.uproject
 UE4PATH=/local/ori/GameEngines/UnrealEngine/Engine
+ARDUPILOT_PATH=/local/dronelabdata/ardupilot
+
 
 
 BUILD_TOOL=Binaries/DotNET/UnrealBuildTool.exe
 #cleanning prev run
 tmux kill-session -t dronelab
-docker ps |grep sitl_image  | cut -d" " -f 1 | xargs docker stop $1
+docker ps -all |grep sitl_image  | cut -d" " -f 1 | xargs docker stop $1
+docker ps -all |grep sitl_image  | cut -d" " -f 1 | xargs docker rm $1
 
 tmux new-session -d -s dronelab
+tmux send-keys "python drone_main.py"
 tmux split-window -h
-tmux send-keys "cd ../../dockers/sitl_image/ && ./run_instance.sh 1" ENTER
+tmux send-keys "cd ../../dockers/sitl_image/ && ./run_instance.sh $ARDUPILOT_PATH 0" ENTER
 tmux split-window -v
 tmux send-keys "export DEMO_DIR=${PWD}" ENTER
 tmux send-keys "cd ${PROJECT_PATH}/Plugins/UE4PyServer/Source/PyServer" ENTER
--- dockers/sitl_image/run_instance.sh
+++ dockers/sitl_image/run_instance.sh
@@ -5,11 +5,11 @@
 #docker run -it --rm --name sitl_run -v $ARDU_PATH:/ardupilot -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY sitl_image /bin/bash
 ( docker ps | grep sitl_run ) && docker rm sitl_run
 docker run -it --rm --name sitl_run \
--v /local/dronelabdata/ardupilot:/ardupilot  \
+-v $1:/ardupilot  \
 -e USERNAME=docker \
 -e USER=docker \
 -e SITL_POSITION_PORT=$( expr 19988 + 10 \* $1 ) \
 -u $UID \
 --net=host \
 sitl_image /bin/bash -c \
-"cd /ardupilot/ArduCopter/ && ../Tools/autotest/sim_vehicle.py -w -I $1"
+"cd /ardupilot/ArduCopter/ && ../Tools/autotest/sim_vehicle.py -w -I $2"
