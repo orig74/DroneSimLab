b986c5a779f932ae6aabd487bbb4363bb479b158
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2016-12-08 14:24:31 +1300

    gazebo working with ros

---
+++ dockers/ros_image_indigo/run_image.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+xhost + local:
+
+#ARDU_PATH=`cd ../../ardupilot && pwd`
+#echo $ARDU_PATH
+#docker run -it --rm --name sitl_run -v $ARDU_PATH:/ardupilot -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY sitl_image /bin/bash
+docker run -it --rm --name ros_indigo \
+-v /local/dronelabdata/PX4:/PX4  \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-e DISPLAY=$DISPLAY \
+-e USERNAME=docker \
+-e USER=docker \
+-e HOME=/home/docker \
+-v /local/dockerhome:/home/docker \
+-u $UID \
+--privileged \
+--net host \
+ros_image_indigo "/bin/bash"
+
+#make posix_sitl_default jmavsim
+
---
+++ scripts/set_dockerhome_dir.sh
@@ -0,0 +1,3 @@
+#TODO: copy config files from config dir
+#TODO: create catkin env for ros based on http://dev.px4.io/ros-mavros-installation.html
+
