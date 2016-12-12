e223c22e4be74c959758404496b2e9c3d1aa4ac1
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2016-12-12 13:17:01 +1300

    update gazebo

--- demos/px4_gazebo/runtmux.sh
+++ demos/px4_gazebo/runtmux.sh
@@ -31,6 +31,7 @@ tmux send-keys 'tmux send-keys "roslaunch mavros px4.launch fcu_url:=\"udp://:14
 #tmux send-keys "cd /PX4/Firmware" ENTER
 tmux send-keys 'tmux select-pane -t 1' ENTER
 tmux send-keys 'tmux send-keys "cd /DroneLab/PX4/Firmware" ENTER' ENTER 
+tmux send-keys 'tmux send-keys "export SITL_POSITION_PORT=11341" ENTER' ENTER 
 tmux send-keys 'tmux send-keys "make posix_sitl_default gazebo" ENTER' ENTER
 tmux send-keys 'tmux select-pane -t 0' ENTER
 #tmux send-keys "tmux send-keys \"source /ros/catkin_ws/devel/setup.bash\" ENTER" ENTER
@@ -39,6 +40,14 @@ tmux send-keys 'tmux send-keys "ROS_PACKAGE_PATH=/ros/catkin_ws/src:$ROS_PACKAGE
 tmux send-keys 'tmux send-keys "cd /ros/catkin_ws && catkin_make" ENTER' ENTER
 tmux send-keys 'tmux send-keys "rosrun control_test offb2.py"' ENTER
 tmux send-keys "tmux att" ENTER
+
+
+tmux new-window -n unreal
+tmux send-keys "export DEMO_DIR=${PWD}" ENTER
+tmux send-keys "cd ${PROJECT_PATH}/Plugins/UE4PyServer/Source/PyServer" ENTER
+tmux send-keys "python config.py --entry_point=unreal_proxy --entry_path=\$DEMO_DIR" ENTER
+
+
 #tmux send-keys "make posix_sitl_default jmavsim" 
 #tmux split-window -v
 #tmux send-keys "export DEMO_DIR=${PWD}" ENTER
