fc0b1fc35c6d31f3927359c9b9ebaa31d01df3e1
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2017-02-15 12:55:42 +1300

    update baked game and testing baked game in px4_gazebo demo including depth camera

--- scripts/run_tmux_ros_px4.sh
+++ scripts/run_tmux_ros_px4.sh
@@ -4,7 +4,7 @@ tmux  new-session -d
 tmux set -g pane-border-status top
 tmux split-window -h
 tmux split-window -v
-tmux send-keys "printf '\033]2;roscore\033\\'" ENTER 
+tmux send-keys "printf '\033]2;roscore\033\\'" ENTER
 tmux send-keys roscore ENTER
 sleep 3
 tmux split-window -v
@@ -15,8 +15,8 @@ tmux send-keys "sleep 3" ENTER #need some delay dont know why
 tmux send-keys "roslaunch mavros px4.launch fcu_url:=udp://:14540@127.0.0.1:14557" ENTER
 tmux select-pane -t 1
 tmux send-keys "printf '\033]2;gazebo\033\\'" ENTER
-tmux send-keys "cd /DroneLab/PX4/Firmware" ENTER 
-tmux send-keys "export SITL_POSITION_PORT=11341" ENTER 
+tmux send-keys "cd /DroneLab/PX4/Firmware" ENTER
+tmux send-keys "export SITL_POSITION_PORT=11341" ENTER
 
 #remark the nexline to load gazebo gui client for debug prpose
 tmux send-keys "export HEADLESS=True" ENTER
@@ -35,7 +35,7 @@ tmux send-keys "printf '\033]2;catkin window\033\\'" ENTER
 tmux send-keys "rosrun control_test ue4_bridge.py" ENTER
 tmux split-window -v
 tmux send-keys "printf '\033]2;catkin window\033\\'" ENTER
-tmux send-keys "rosrun image_view image_view image:=rgb_camera_0" ENTER
+#tmux send-keys "rosrun image_view image_view image:=rgb_camera_0" ENTER
 tmux split-window -v
 tmux send-keys "printf '\033]2;fdm zmq publish to ue4\033\\'" ENTER
 tmux send-keys "cd /DroneLab/scripts/" ENTER
@@ -44,4 +44,3 @@ tmux send-keys "PATH=/miniconda/bin python fdm_pub_px4_gazebo.py --config_path=$
 tmux select-pane -t 0
 tmux set -g pane-border-format "#{pane_index} #T"
 tmux att
-
