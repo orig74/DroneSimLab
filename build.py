eb28199760899f181869cc0e8df381125cfed5b3
ori ganoni <ori.ganoni@pg.canterbury.ac.nz>
2017-07-17 10:09:56 +1200

    replacing asserts with run_shell command

--- build.py
+++ build.py
@@ -22,6 +22,12 @@ if not args.skip_ue4_editor:
 games_path='https://studweb.cosc.canterbury.ac.nz/~oga13/ue4_games/'
 games_names=['game_demo']
 
+def run_shell(cmd,failmsg):
+    ret=os.system(cmd)
+    if ret!=0:
+        print('Error: ',failmsg)
+        sys.exit(-1)
+
 print("update submodules...")
 
 submodules=map(lambda x:x.split('=')[1].strip(),os.popen('grep path .gitmodules').readlines())
@@ -29,7 +35,10 @@ for submodule in submodules:
     if args.skip_ue4_editor and 'UnrealEngine' in submodule:
         continue
     print('updating submodule:', submodule)
-    assert(os.system("git submodule update --init --recursive "+submodule)==0)
+    run_shell("git submodule update --init --recursive "+submodule,
+        'failed updataing submodules.. please check you connection')
+    
+
 
 print("downloading baked games...")
 
@@ -42,27 +51,31 @@ for game_name in games_names:
     if not os.path.isfile(game_local_dir+'/done'):
         cmd="curl -o "+game_local_dir+'/'+game_name+'.tgz '+games_path+'/'+game_name+'.tgz'
         print('Downloading command:',cmd) 
-        assert(os.system(cmd)==0)
-        assert(os.system("cd "+game_local_dir+" && tar xzf "+game_name+'.tgz')==0)
-        assert(os.system("rm "+game_local_dir+'/'+game_name+'.tgz')==0)
+        run_shell(cmd,'Failed downloading game '+game_name)
+        run_shell("cd "+game_local_dir+" && tar xzf "+game_name+'.tgz','fail extracting game')
+        run_shell("rm "+game_local_dir+'/'+game_name+'.tgz','fail rm')
         open(game_local_dir+'/done','w')
 
 print("building docker images...")
 
-assert(os.system("docker images > /dev/null")==0) #just to see if have the right preveliges
+run_shell("docker images > /dev/null",'Fail running docker images, do you have the right previlages?'
+        '\ntry running:\ndocker images') 
 current_docker_images=map(lambda x:x.strip(),os.popen('docker images |cut -d" " -f 1').readlines())
 
 if 'unreal_engine_4' in req_docker_images:
-    assert(os.system("cd dockers/unreal_engine_4 && ./prep_unreal")==0)
+    run_shell("cd dockers/unreal_engine_4 && ./prep_unreal",'Error: faild to prepare unreal for installation')
+    
 
 for docker_image in req_docker_images:
     if docker_image not in current_docker_images:
         print('building image ',docker_image)
-        assert(os.system('cd dockers/'+docker_image+' && ../build')==0) 
+        run_shell('cd dockers/'+docker_image+' && ../build > /tmp/dbuild.log 2>&1',
+            'Error: failed bulding docker image '+docker_image+' please look at the log file /tmp/dbuild.log')
 
 print("install ros...")
 #if not os.path.isdir('ros/catkin_mavros'):
 #    os.mkdir('ros/catkin_mavros')
-assert(os.system('''cd dockers/ros_image_indigo && ./run_image.sh '-c "/DroneLab/scripts/make_catkin.sh"' ''')==0)
+run_shell('''cd dockers/ros_image_kinetic && ./run_image.sh '-c "/DroneLab/scripts/make_catkin.sh"' ''',
+        'Error: faild installing ros')
 
 print("done!")
