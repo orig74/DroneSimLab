1f7116c7d8c4ad9dabdef8a0ecd482b122519362
Ori <ori.ganoni@gmail.com>
2017-08-23 07:19:25 +1200

    Update to install ROS kinetic as default in the build script

--- scripts/make_catkin.sh
+++ scripts/make_catkin.sh
@@ -2,7 +2,8 @@
 #based on
 #http://dev.px4.io/ros-mavros-installation.html
 if [ ! -d /DroneLab/ros/catkin_ws/build ] ; then
-source /opt/ros/indigo/setup.bash
+#source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 cd /DroneLab/ros/catkin_ws
 catkin_make install && cd build && make install
 else
