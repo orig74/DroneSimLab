e17dca1afef819bee147b8daa1853fac7367ebfb
labuser <labuser@cs19123bs.canterbury.ac.nz>
2019-03-08 20:14:05 +1300

    starting support for 18.04

--- dockers/install_graphic_driver.sh
+++ dockers/install_graphic_driver.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-apt-get -y update
-DRIVER_NAME=`apt-cache search nvidia |grep $GDRIVER | head -n1 |cut -d" " -f1`
+##apt-get -y update
+#DRIVER_NAME=`apt-cache search nvidia |grep $GDRIVER | head -n1 |cut -d" " -f1`
+DRIVER_NAME=""
 
+add-apt-repository -y ppa:graphics-drivers/ppa
+apt update
+DRIVER_NAME=$(python3 << END
+import os,sys
+nvidia_pkgs=os.popen('apt-cache search nvidia- | grep driver | cut -d " " -f 1').read().split('\n')
+for pkg in nvidia_pkgs:
+  if pkg.startswith('nvidia-') and "$GDRIVER" in os.popen('apt-cache show package '+pkg+' |grep Version').read():
+     print(pkg)
+     print('found nvidia: '+pkg,file=sys.stderr)
+     break
+  else:
+     print(pkg,file=sys.stderr)
+END
+)
 
-if [ "$DRIVER_NAME" == "" ] ; then
-	add-apt-repository -y ppa:graphics-drivers/ppa
-	apt update
-	DRIVER_NAME=`apt search nvidia |grep $GDRIVER | head -n1 |cut -d"/" -f1`
-fi
+echo "************** DRIVER_NAME= " $DRIVER_NAME
+
 if [ "$DRIVER_NAME" == "" ] ; then
 	echo "Error nvidia Driver $GDRIVER not found on guest machine" >&2
 	echo "Try install the latest nvidia driver (using ppa) on your host machine" >&2
